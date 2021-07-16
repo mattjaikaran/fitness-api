07ea3fa9bec1edda48f6ec94222f73c1b732b63d
mattjaikaran <mattjaikaran@gmail.com>
2021-07-16 15:45:08 -0400

    updated admin controller logic

--- app/Controllers/Http/AdminController.ts
+++ app/Controllers/Http/AdminController.ts
@@ -7,26 +7,67 @@ export default class AdminController {
     return { message: 'Admin Controller Index' }
   }
 
-  // Get All Fitness Classes
+  // Get All Locations
+  public async getAllLocations() {}
   // Get All Boxes
-  // Get Fitness Class by ID/Type/Instructor
-  // Get an Instructor/Student/Admin User by ID
-  // Get a Box by ID
-  // Create new Instructor/Student/Admin User
-  // Create new Location
-  // Create new Box
-  // Create new Fitness Class
-  // Update Fitness Class
-  // Update Box 
-  // Update Student/Instructor/Admin User 
-  // Delete a Fitness Class/Box reservation
-  // Delete a Student/Instructor/Admin User
+  public async getAllBoxes() {}
+  // Get All Fitness Classes
+  public async getAllFitnessClasses() {}
+  // Get ALL Users (All Instructors, All Students, All Admin Users)
+  public async getAllUsers() {}
+  // Get All Instructors
+  public async getAllInstructors() {}
+  // Get All Students
+  public async getAllStudents() {}
+  // Get All Admin Users
+  public async getAllAdminUsers() {}
+  // Get a Location by ID/Name
+  public async getLocationById() {}
+  // Get a Box by ID/Name
+  public async getBoxById() {}
+  // Get a Box by Status/Availability
+  public async getBoxByStatus() {}
+  // Get all Students in Fitness Class
+  public async getAllStudentsInClass() {}
+  // Get a Fitness Class by ID/Name/Type/Instructor
+  public async getFitnessClassById() {}
+  public async getFitnessClassByName() {}
+  public async getFitnessClassByType() {}
+  public async getFitnessClassByInstructor() {}
+  // Get a User's Fitness Class History
+  public async getUserClassHistory() {}
+  // Get an Instructor/Student/Admin User by ID/Name
+  public async getUserById() {}
+  // Create a new Location
+  public async createLocation() {}
+  // Create a new Box
+  public async createBox() {}
+  // Create a new Fitness Class
+  public async createFitnessClass() {}
+  // Create a new Instructor/Student/Admin User
+  public async createUser() {}
+  // Update a Location
+  public async updateLocation() {}
+  // Update a Box 
+  public async updateBox() {}
+  // Update a Fitness Class
+  public async updateFitnessClass() {}
+  // Update a Student/Instructor/Admin User 
+  public async updateUser() {}
+  // Delete a Location
+  public async deleteLocation() {}
   // Delete a Box
+  public async deleteBox() {}
+  // Delete a Fitness Class/Box reservation
+  public async deleteBoxReservation() {}
+  // Delete a Student/Instructor/Admin User
+  public async deleteUser() {}
 
   // Admin User approves a Box. PUT/PATCH Request
   public async approveBox() {
-    // Get a Fitness class by Box approval 'Pending' 
+    // Get a Fitness class by Box Reservation Status = 'Pending' 
     // Update from Pending to Confirmed/Cancelled. 
+    // Change available to false.
     // Return updated Fitness Class. 
     // Message: `Class is ${boxStatus}. ${fitnessClass} is at ${DateTime}`
   }
